c32515bb242c562932acf7fba90da746e4587e3b
jlord <95568851+Jaylord-Bucayu@users.noreply.github.com>
2022-12-15 14:08:53 +0800

    Add files via upload

---
+++ Snake_Ladder_Game/Snake_Ladder_Game/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Image1 = New System.Windows.Forms.PictureBox()
+        Me.Dice = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Dice2 = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Image2 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Image1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Dice, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Dice2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Image2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(48, 57)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(807, 657)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(900, 306)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 70)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Player 1 Role Dice"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(900, 423)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 70)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Player 2 Role Dice"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Image1
+        '
+        Me.Image1.BackColor = System.Drawing.Color.White
+        Me.Image1.BackgroundImage = CType(resources.GetObject("Image1.BackgroundImage"), System.Drawing.Image)
+        Me.Image1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Image1.Location = New System.Drawing.Point(72, 655)
+        Me.Image1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Image1.Name = "Image1"
+        Me.Image1.Size = New System.Drawing.Size(45, 43)
+        Me.Image1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Image1.TabIndex = 3
+        Me.Image1.TabStop = False
+        '
+        'Dice
+        '
+        Me.Dice.BackColor = System.Drawing.Color.White
+        Me.Dice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Dice.Location = New System.Drawing.Point(900, 57)
+        Me.Dice.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Dice.Name = "Dice"
+        Me.Dice.Size = New System.Drawing.Size(227, 199)
+        Me.Dice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Dice.TabIndex = 4
+        Me.Dice.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Gold
+        Me.Label1.Location = New System.Drawing.Point(44, 748)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 36)
+        Me.Label1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(1028, 334)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 16)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Position 0"
+        '
+        'Dice2
+        '
+        Me.Dice2.BackColor = System.Drawing.Color.White
+        Me.Dice2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Dice2.Location = New System.Drawing.Point(900, 514)
+        Me.Dice2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Dice2.Name = "Dice2"
+        Me.Dice2.Size = New System.Drawing.Size(227, 199)
+        Me.Dice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Dice2.TabIndex = 7
+        Me.Dice2.TabStop = False
+        '
+        'Timer2
+        '
+        '
+        'Image2
+        '
+        Me.Image2.BackColor = System.Drawing.Color.Black
+        Me.Image2.BackgroundImage = CType(resources.GetObject("Image2.BackgroundImage"), System.Drawing.Image)
+        Me.Image2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Image2.Location = New System.Drawing.Point(72, 671)
+        Me.Image2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Image2.Name = "Image2"
+        Me.Image2.Size = New System.Drawing.Size(45, 43)
+        Me.Image2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Image2.TabIndex = 8
+        Me.Image2.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(1028, 450)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 16)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Position 0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1185, 822)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Image2)
+        Me.Controls.Add(Me.Dice2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Dice)
+        Me.Controls.Add(Me.Image1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Image1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Dice, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Dice2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Image2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Image1 As PictureBox
+    Friend WithEvents Dice As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Dice2 As PictureBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Image2 As PictureBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Snake_Ladder_Game/Snake_Ladder_Game/Form1.vb
@@ -0,0 +1,300 @@
+﻿Public Class Form1
+
+    Dim n As Integer
+
+    Dim n2 As Integer
+
+    Dim player1Cell As Integer
+    Dim player2Cell As Integer
+
+
+    Dim t2 As Integer
+    Dim t As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Button1.Visible = False
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+        t = 0
+
+        Do
+            Dice.Refresh()
+
+            Dim MyPen As Pen
+
+
+            MyPen = New Pen(Drawing.Color.DarkCyan, 10)
+            Dim MyGraphics As Graphics = Dice.CreateGraphics
+            n = Int(6 * Rnd()) + 1
+
+            Select Case n
+                Case 1
+                    MyGraphics.DrawEllipse(MyPen, 80, 80, 10, 10)
+                Case 2
+                    MyGraphics.DrawEllipse(MyPen, 40, 40, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 120, 10, 10)
+                Case 3
+                    MyGraphics.DrawEllipse(MyPen, 40, 40, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 80, 80, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 120, 10, 10)
+                Case 4
+                    MyGraphics.DrawEllipse(MyPen, 40, 40, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 40, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 40, 120, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 120, 10, 10)
+                Case 5
+                    MyGraphics.DrawEllipse(MyPen, 40, 40, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 40, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 80, 80, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 40, 120, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 120, 10, 10)
+                Case 6
+                    MyGraphics.DrawEllipse(MyPen, 40, 40, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 40, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 40, 80, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 80, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 40, 120, 10, 10)
+                    MyGraphics.DrawEllipse(MyPen, 120, 120, 10, 10)
+
+
+
+            End Select
+            t = t + 1
+
+
+            Label1.Text = n.ToString() + " plus move"
+
+        Loop Until t > 1000
+        player1Cell += n
+
+
+
+        If (player1Cell = 9) Then
+            Image1.Top = 430
+            Image1.Left -= 60
+            player1Cell = 28
+
+        ElseIf (player1Cell = 33) Then
+            Image1.Top = 380
+            Image1.Left -= 60 * (n2 - 1)
+
+        ElseIf (player1Cell = 38) Then
+
+            Image1.Left -= 60 * n2
+
+        ElseIf (player1Cell = 42) Then
+            Image1.Top = 320
+            Image1.Left -= 60 * (n2 - 1)
+
+        ElseIf (player1Cell = 51) Then
+            Image1.Top = 270
+            Image1.Left += 120
+
+
+        ElseIf (player1Cell = 57) Then
+            Image1.Left -= 60 * (n2 + 2)
+        ElseIf (player1Cell = 60) Then
+            Image1.Left -= 60 * n
+
+        ElseIf (player1Cell = 62) Then
+            Image1.Top = 220
+            Image1.Left += 60 * (n - 1)
+
+        Else
+            Image1.Left += 60 * n
+        End If
+
+
+
+
+        Label2.Text = "Position " + player1Cell.ToString()
+
+
+
+        Timer1.Stop()
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Visible = False
+        Button2.Visible = True
+        Timer2.Stop()
+
+        Timer1.Start()
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button2.Visible = False
+        Button1.Visible = True
+        Timer1.Stop()
+
+        Timer2.Start()
+
+
+
+
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+
+        t2 = 0
+
+        Do
+            Dice2.Refresh()
+
+            Dim MyPen2 As Pen
+
+
+            MyPen2 = New Pen(Drawing.Color.DarkCyan, 10)
+            Dim MyGraphics2 As Graphics = Dice2.CreateGraphics
+            n2 = Int(5 * Rnd()) + 2
+
+            Select Case n2
+                Case 1
+                    MyGraphics2.DrawEllipse(MyPen2, 80, 80, 10, 10)
+                Case 2
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 40, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 120, 10, 10)
+                Case 3
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 40, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 80, 80, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 120, 10, 10)
+                Case 4
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 40, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 40, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 120, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 120, 10, 10)
+                Case 5
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 40, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 40, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 80, 80, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 120, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 120, 10, 10)
+                Case 6
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 40, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 40, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 80, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 80, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 40, 120, 10, 10)
+                    MyGraphics2.DrawEllipse(MyPen2, 120, 120, 10, 10)
+
+
+
+            End Select
+            t2 += 1
+
+
+            Label1.Text = n2.ToString() + " plus move"
+        Loop Until t2 > 1000
+        player2Cell += n2
+
+
+
+        If (player2Cell = 9) Then
+            Image2.Top = 430
+            Image2.Left += 60
+            player2Cell = 28
+
+
+        ElseIf (player2Cell = 33) Then
+
+            Image2.Top = 380
+
+
+
+        ElseIf (player2Cell = 36) Then
+
+            Image2.Left -= 60 * n2
+
+        ElseIf (player2Cell = 39) Then
+            Image2.Left -= 60 * n2
+
+        ElseIf (player2Cell = 44) Then
+            Image2.Top = 320
+            Image2.Left += 120
+
+        ElseIf (player2Cell = 52) Then
+            Image2.Top = 270
+
+        ElseIf (player2Cell = 55) Then
+            Image2.Left -= 60 * n2
+
+        ElseIf (player2Cell = 60) Then
+            Image2.Left -= 60 * n2
+
+
+        ElseIf (player2Cell = 64) Then
+            Image2.Top = 220
+            Image2.Left += 60 * (n2 - 1)
+
+        ElseIf (player2Cell = 72) Then
+            Image2.Top = 170
+            Image2.Left += 60 * (n2 - 3)
+
+        ElseIf (player2Cell = 78) Then
+            Image2.Top = 50
+            Image2.Left -= 60 * (n2 - 1)
+            player2Cell = 97
+        ElseIf (player2Cell > 97) Then
+
+            Image2.Left -= 60 * n2
+
+            Timer1.Stop()
+            Timer2.Stop()
+
+            Dim result As DialogResult = MessageBox.Show("Player 2 Wins", "Snake and Ladder", MessageBoxButtons.YesNo)
+            If result = DialogResult.No Then
+                MessageBox.Show("Cancel pressed")
+
+
+
+            ElseIf result = DialogResult.Yes Then
+                player1Cell = 0
+                player2Cell = 0
+                t = 0
+                t2 = 0
+                n = 0
+                n2 = 0
+                Image1.Top = 530
+                Image1.Left -= 60 * 4
+                Image2.Left = 60
+                Image2.Top = 530
+            End If
+
+
+
+
+        Else
+            Image2.Left += 60 * n2
+        End If
+
+
+
+
+        Label3.Text = "Position " + player2Cell.ToString()
+
+
+
+
+        Timer2.Stop()
+
+
+
+    End Sub
+End Class
